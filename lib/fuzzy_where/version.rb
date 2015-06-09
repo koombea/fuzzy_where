a4245a4c5c3640603d5bb84603d31822fc39ed38
Gustavo Bazan <gustavo.bazan@koombea.com>
2015-06-09 11:35:25 -0500

    Fix generate config

--- lib/fuzzy_where/version.rb
+++ lib/fuzzy_where/version.rb
@@ -1,4 +1,4 @@
 module FuzzyWhere
   # Gem Version
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
--- lib/generators/fuzzy_where/templates/fuzzy_record_config.rb
+++
@@ -1,4 +0,0 @@
-FuzzyWhere.configure do |config|
-  # config.where_method_name = :fuzzy_where
-  config.predicates_file = Rails.root.join('config', 'fuzzy_predicates.yml')
-end
